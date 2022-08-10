050cc0ed8490ce9c27f50de33724bf9e156c933b
Daniel Louvier <13359249+dlouvier@users.noreply.github.com>
2022-08-10 12:46:58 +0000

    remove test

--- client/collection_graph_test.go
+++ client/collection_graph_test.go
@@ -36,39 +36,4 @@ func TestCollectionGraph(t *testing.T) {
 
 		assert.Equal(t, expected, pg)
 	})
-
-	t.Run("Update collection graph", func(t *testing.T) {
-		collectionGraphToBeUpdated := CollectionGraph{
-			Revision: 2,
-			Groups: map[string]map[string]string{
-				"1": {
-					"1":  "none",
-					"10": "write",
-					"9":  "none",
-				},
-			},
-		}
-		expectedCollectionGraph := CollectionGraph{
-			Revision: 1,
-			Groups: map[string]map[string]string{
-				"1": {
-					"1": "none",
-				},
-			},
-		}
-
-		httpMethod := http.MethodPut
-		svr := server("/api/collection/graph", httpMethod, expectedCollectionGraph)
-		defer svr.Close()
-
-		c := Client{
-			BaseURL:    svr.URL,
-			HTTPClient: &http.Client{},
-		}
-
-		updated, err := c.UpdateCollectionGraph(collectionGraphToBeUpdated)
-
-		assert.Nil(t, err)
-		assert.Equal(t, expectedCollectionGraph, updated)
-	})
 }
