6deb875db8cbe8a3618ea0838cb04c240604b32a
Robert Millard <robert.millard@getmoss.com>
2023-06-02 16:24:38 +0200

    mock server for graphUpdate

--- client/collection_graph_test.go
+++ client/collection_graph_test.go
@@ -37,3 +37,44 @@ func TestCollectionGraph(t *testing.T) {
 		assert.Equal(t, expected, pg)
 	})
 }
+
+func TestUpdateCollectionGraph(t *testing.T) {
+	t.Run("Update collection graph", func(t *testing.T) {
+		updatedExpected := CollectionGraph{
+			Revision: 2,
+			Groups: map[string]map[string]string{
+				"1": {
+					"1":  "none",
+					"10": "read",
+					"9":  "write",
+				},
+				"4": {
+					"1":  "none",
+					"10": "none",
+					"9":  "none"},
+			},
+		}
+
+		svr := graphServer()
+		defer svr.Close()
+
+		c := Client{
+			BaseURL:    svr.URL,
+			HTTPClient: &http.Client{},
+		}
+
+		// Update the graph
+		_, err := c.UpdateCollectionGraph(updatedExpected)
+		if err != nil {
+			t.Fatalf("Failed to update collection graph: %v", err)
+		}
+
+		// Get the graph after update
+		actualUpdatedCG, err := c.GetCollectionGraph()
+		if err != nil {
+			t.Fatalf("Failed to get collection graph: %v", err)
+		}
+
+		assert.Equal(t, updatedExpected, actualUpdatedCG)
+	})
+}
--- client/testutils.go
+++ client/testutils.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 )
@@ -16,6 +17,52 @@ func server(url string, httpMethod string, expected interface{}) *httptest.Serve
 			w.WriteHeader(http.StatusBadRequest)
 		}
 	})
+	svr := httptest.NewServer(mux)
+	return svr
+}
+
+func graphServer() *httptest.Server {
+	mux := http.NewServeMux()
+
+	// Maintain an in-memory CollectionGraph that starts with a default value
+	cg := CollectionGraph{
+		Revision: 1,
+		Groups: map[string]map[string]string{
+			"1": {
+				"1":  "read",
+				"10": "write",
+				"9":  "none",
+			},
+			"4": {
+				"1":  "none",
+				"10": "none",
+				"9":  "none"},
+		},
+	}
+
+	mux.HandleFunc("/api/collection/graph", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			_ = json.NewEncoder(w).Encode(cg)
+		case http.MethodPut:
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			var newCg CollectionGraph
+			err = json.Unmarshal(body, &newCg)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			cg = newCg
+			w.WriteHeader(http.StatusOK)
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	})
+
 	svr := httptest.NewServer(mux)
 	return svr
 }
